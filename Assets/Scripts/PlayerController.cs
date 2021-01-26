4aca75a55ee7bf99ff93ce1f23d3e186dfa87b1a
CHEMS ED-DINE RACHIDI <chemsed.dinerachidi@gmail.com>
2021-01-26 23:48:03 +0100

    Updated player model with animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,41 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     float movHor;
     float movVer;
     Vector3 movement;
     Rigidbody rb;
     [SerializeField] float speed = 1000f;
-
+    Animator animator;
+    Camera cam;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        Movement();
-        Shoot();
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+        animator = GetComponentInChildren<Animator> ();
+        cam = Camera.main;
     }
 
-    void Movement()
-    {
-        movHor = Input.GetAxis("Horizontal");
-        movVer = Input.GetAxis("Vertical");
-        movement = new Vector3(movHor, 0, movVer);
+    // Update is called once per frame
+    void Update () {
+        Movement ();
+        Shoot ();
+    }
+
+    void Movement () {
+        movHor = Input.GetAxis ("Horizontal");
+        movVer = Input.GetAxis ("Vertical");
+        movement = new Vector3 (movHor, 0, movVer);
         rb.velocity = movement * Time.deltaTime * speed;
         //rb.AddForce(movement * Time.fixedDeltaTime * speed, ForceMode.Impulse);
+
+        Vector3 vel = rb.velocity;
+        if (vel.magnitude == 0) {
+            animator.SetTrigger ("Idle");
+        } else {
+            animator.SetTrigger ("Run");
+        }
     }
-    void Shoot()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+    void Shoot () {
+        if (Input.GetKeyDown (KeyCode.Space)) {
             //Throw Bomb
+            animator.SetTrigger ("Attack");
         }
     }
 }
